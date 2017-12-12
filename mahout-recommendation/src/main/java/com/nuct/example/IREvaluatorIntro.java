11151e54171f7256db1f5f160a246377f002462d
zhouning <ncutits@163.com>
2017-12-12 18:22:06 +0800

    no message

--- mahout-recommendation/src/main/java/com/nuct/example/IREvaluatorIntro.java
+++ mahout-recommendation/src/main/java/com/nuct/example/IREvaluatorIntro.java
@@ -1,10 +1,10 @@
 package com.nuct.example;
 
 import org.apache.mahout.cf.taste.common.TasteException;
+import org.apache.mahout.cf.taste.eval.IRStatistics;
 import org.apache.mahout.cf.taste.eval.RecommenderBuilder;
-import org.apache.mahout.cf.taste.eval.RecommenderEvaluator;
-import org.apache.mahout.cf.taste.impl.eval.AverageAbsoluteDifferenceRecommenderEvaluator;
-import org.apache.mahout.cf.taste.impl.eval.RMSRecommenderEvaluator;
+import org.apache.mahout.cf.taste.eval.RecommenderIRStatsEvaluator;
+import org.apache.mahout.cf.taste.impl.eval.GenericRecommenderIRStatsEvaluator;
 import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
 import org.apache.mahout.cf.taste.impl.neighborhood.NearestNUserNeighborhood;
 import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
@@ -21,35 +21,30 @@ import java.io.File;
  * Created by zhouning on 2017/12/12.
  * desc:
  */
-public class EvaluatorIntro {
-    private EvaluatorIntro() {
+public class IREvaluatorIntro {
+    private IREvaluatorIntro() {
     }
 
     public static void main(String[] args) throws Exception {
-        RandomUtils.useTestSeed();//用来使得每次随机都一样，仅限用于测试，不要在实际中使用
+        RandomUtils.useTestSeed();
 
-        final DataModel model = new FileDataModel(new File("data/ua.base"));
-        //计算平均差值
-        RecommenderEvaluator evaluator = new AverageAbsoluteDifferenceRecommenderEvaluator();
-        //计算均方根差
-        RecommenderEvaluator recommenderEvaluator = new RMSRecommenderEvaluator();
+        DataModel model = new FileDataModel(new File("data/ua.base"));
+        RecommenderIRStatsEvaluator evaluator = new GenericRecommenderIRStatsEvaluator();
 
         RecommenderBuilder recommenderBuilder = new RecommenderBuilder() {
             @Override
             public Recommender buildRecommender(DataModel dataModel) throws TasteException {
-                //用户相似度，使用基于皮尔逊相关系数计算相似度
                 UserSimilarity similarity = new PearsonCorrelationSimilarity(dataModel);
-                //对每个用户取固定数量N个最近邻居
                 UserNeighborhood neighborhood = new NearestNUserNeighborhood(100, similarity, dataModel);
                 return new GenericUserBasedRecommender(dataModel, neighborhood, similarity);
             }
         };
-        //指标，约接近0，效果越好
-        double score = evaluator.evaluate(recommenderBuilder, null, model, 0.7, 1.0);
-        double rmse = recommenderEvaluator.evaluate(recommenderBuilder, null, model, 0.7, 1.0);
-        System.out.println(score);
-        System.out.println(rmse);
+
+        //推荐5个，
+        IRStatistics statistics = evaluator.evaluate(recommenderBuilder, null, model, null, 5, GenericRecommenderIRStatsEvaluator.CHOOSE_THRESHOLD, 1.0);
+
+        System.out.println(statistics.getPrecision());
+        System.out.println(statistics.getRecall());
+        System.out.println(statistics.getF1Measure());
     }
-
-
 }
--- mahout-recommendation/src/main/java/com/nuct/example/RecommenderIntro.java
+++ mahout-recommendation/src/main/java/com/nuct/example/RecommenderIntro.java
@@ -1,15 +1,39 @@
 package com.nuct.example;
 
+import org.apache.mahout.cf.taste.common.TasteException;
+import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
+import org.apache.mahout.cf.taste.impl.neighborhood.NearestNUserNeighborhood;
+import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
+import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
+import org.apache.mahout.cf.taste.model.DataModel;
+import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
+import org.apache.mahout.cf.taste.recommender.RecommendedItem;
+import org.apache.mahout.cf.taste.recommender.Recommender;
+import org.apache.mahout.cf.taste.similarity.UserSimilarity;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
 /**
  * Created by zhouning on 2017/12/12.
  * desc:
  */
 public class RecommenderIntro {
 
-    private RecommenderIntro(){
+    private RecommenderIntro() {
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, TasteException {
+        DataModel model = new FileDataModel(new File("data/ua.base"));
+        UserSimilarity similarity = new PearsonCorrelationSimilarity(model);
+        UserNeighborhood neighborhood = new NearestNUserNeighborhood(100, similarity, model);
+        Recommender recommender = new GenericUserBasedRecommender(model, neighborhood, similarity);
 
+        //给用户1推荐20个物品
+        List<RecommendedItem> recommendedItems = recommender.recommend(1, 20);
+        for (RecommendedItem recommendedItem : recommendedItems) {
+            System.out.println(recommendedItem);
+        }
     }
 }
