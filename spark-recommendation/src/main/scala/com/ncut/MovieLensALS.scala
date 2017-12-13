e7d279f5aee8690b723aa7b934ab9b2d610f42b8
zhouning <ncutits@163.com>
2017-12-13 18:42:20 +0800

    no message

---
+++ spark-recommendation/src/main/scala/com/ncut/MovieLensALS.scala
@@ -0,0 +1,31 @@
+package com.ncut
+
+/**
+  * Created by zhouning on 2017/12/13.
+  * desc: 
+  */
+class MovieLensALS {
+
+  //1. Define a rating elicitation function
+
+  //2. Define a RMSE computation function
+
+  //3. Main
+
+  //3.1 Setup env
+
+  //3.2 Load ratings data and know your data
+
+  //3.3 Elicitate personal rating
+
+  //3.4 Split data into train(60%), validation(20%) and test(20%)
+
+  //3.5 Train model and optimize model with validation set
+
+  //3.6 Evaluate model on test set
+
+  //3.7 Create a baseline and compare it with best model
+
+  //3.8 Make a personal recommendation
+
+}
